18cb6e07263b5928d93321982e89d86cca99c63e
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-06-05 08:56:09 -0700

    Create tab component

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,33 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+// Grab the entry point div we're going to attach the new topics
+const trendingTopics = document.querySelector(".topics");
+
+// Get the data and create a tab for each topic
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then((response) => {
+    const topics = response.data.topics;
+    topics.forEach((topic) => {
+      trendingTopics.appendChild(createTab(topic));
+    });
+  })
+  .catch((error) => {
+    console.log("There's an error: ", error);
+  });
+
+// Create a Tab-making machine
+function createTab(tabName) {
+  // Create the HTML elements
+  const tabDiv = document.createElement("div");
+
+  // Match the corresponding class
+  tabDiv.classList.add("tab");
+
+  // Add the data inside the tab
+  tabDiv.textContent = tabName;
+
+  return tabDiv;
+}
