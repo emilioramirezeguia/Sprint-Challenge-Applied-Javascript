00f787559942ebc20c0d3877a736fc5ad9d03996
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-06-05 09:47:31 -0700

    Create initial Article component functionality

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,61 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log("There's an error somewhere: ", error);
+  });
+
+// Create an Article-making machine
+function createArticle(articleObject) {
+  // Grab the elements from the articleObject parameter and save for later
+  const headline = articleObject.headline;
+  const authorName = articleObject.authorName;
+  const authorPhoto = articleObject.authorPhoto;
+
+  // Create HTML elements where data is going to be placed
+  const articleDiv = document.createElement("div");
+  const headlineDiv = document.createElement("div");
+  const authorDiv = document.createElement("div");
+  const authorImageDiv = document.createElement("div");
+  const authorImage = document.createElement("img");
+  const authorNameSpan = document.createElement("span");
+
+  // Add corresponding classes to each element
+  articleDiv.classList.add(".card");
+  headlineDiv.classList.add(".headline");
+  authorDiv.classList.add(".author");
+  authorImageDiv.classList.add(".img-container");
+
+  // Nest HTML elements appropriately
+  articleDiv.appendChild(headlineDiv);
+  articleDiv.appendChild(authorDiv);
+  authorDiv.appendChild(authorImageDiv);
+  authorDiv.appendChild(authorNameSpan);
+  authorImageDiv.appendChild(authorImage);
+
+  // Insert text in the correct places
+  headlineDiv.textContent = headline;
+  authorImage.src = authorPhoto;
+  authorNameSpan.textContent = `By ${authorName}`;
+
+  return articleDiv;
+}
+
+// Test the component is working
+const testArticle = {
+  headline: "Emilio's headline",
+  authorName: "Emilio the Author",
+  authorPhoto: "https://picsum.photos/200",
+};
+
+// Create the entry point to attach articles
+const articles = document.querySelector(".cards-container");
+const newArticle = createArticle(testArticle);
+console.log(newArticle);
+articles.appendChild(newArticle);
